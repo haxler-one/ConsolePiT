7625c356e83e2a3ee5a9ef3d93f8d226d87a0f2e
User <User@WIN-1GF78P49AAA>
2025-02-27 23:21:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleFibonacci/ProgramFibonacci.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class ProgramFibonacci
+{
+    static void Main(string[] args)
+    {
+        int result = Fibonacci(5);
+        Console.WriteLine(result);
+    }
+
+    static int Fibonacci(int n)
+    {
+        if (n == 0) return 0; // Обработка случая, когда n = 0
+        if (n == 1) return 1; // Обработка случая, когда n = 1
+
+        int n1 = 0;
+        int n2 = 1;
+        int sum = 0;
+
+        for (int i = 2; i <= n; i++)
+        {
+            sum = n1 + n2;
+            n1 = n2;
+            n2 = sum;
+        }
+
+        return n2; // Возвращаем n2, который содержит n-ое число Фибоначчи
+    }
+}
+
---
+++ ConsoleLetters/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleLetters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleLetters")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ce2f3e97-69c6-4c9f-a8aa-fa6bf0e21f66")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
